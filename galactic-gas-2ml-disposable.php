513f30887b1af560087035c34b090ac5fb20de19
EMANI SAI KIRAN REDDY <112548987+SAIKIRANREDDY0014@users.noreply.github.com>
2024-01-16 18:18:18 +0530

    Add files via upload

---
+++ galactic-gas-2ml-disposable.php
@@ -0,0 +1,148 @@
+<!doctype html>
+<html lang="en">
+  <head>
+ <title>Modern Distribution</title>
+  <link type="text/css" href="css/owl.carousel.css" rel="stylesheet">
+  <style media="screen">
+    .cat-card{margin-top: 30px;}
+  </style>
+   <?php require_once 'header-product.php';?>
+
+
+
+   <div class="breadcrumb-area pt-30 pb-39">
+           <div class="container">
+               <div class="breadcrumb-content ">
+
+                   <ul style="color:#000000">
+                     <li  style="color:#000000"><a href="http://moderndistributionnc.com"  style="color:#000000">home </a>/ </li>
+                      <li  style="color:#000000"><a href="disposables.php"  style="color:#000000"> Disposable  </a>/</li>
+                       <li  style="color:#000000">Galactic Gas - THCA+THCP+HHC+LIVE RESIN 2ML Disposable</li>
+                   </ul>
+               </div>
+           </div>
+       </div>
+   <div class="product-details ptb-30 pb-90">
+       <div class="container">
+           <div class="row">
+               <div class="col-md-12 col-lg-6 col-12">
+                   <div class="product-details-img-content">
+                       <div class="product-details-tab mr-70">
+                           <div class="product-details-large tab-content">
+                               <div class="tab-pane active show fade" id="pro-details1" role="tabpanel">
+                                   <div class="easyzoom easyzoom--overlay">
+                                       <a href="images/thca/Galactic-Gas-1.webp">
+                                           <img src="images/thca/Galactic-Gas-1.webp" alt="">
+                                       </a>
+                                   </div>
+                               </div>
+                               <div class="tab-pane fade" id="pro-details2" role="tabpanel">
+                                   <div class="easyzoom easyzoom--overlay">
+                                       <a href="images/thca/Galactic-Gas-2.webp">
+                                           <img src="images/thca/Galactic-Gas-2.webp" alt="">
+                                       </a>
+                                   </div>
+                               </div>
+                               <div class="tab-pane fade" id="pro-details3" role="tabpanel">
+                                   <div class="easyzoom easyzoom--overlay">
+                                       <a href="images/thca/Galactic-Gas-3.webp">
+                                           <img src="images/thca/Galactic-Gas-3.webp" alt="">
+                                       </a>
+                                   </div>
+                               </div>
+
+                           </div>
+                           <div class="product-details-small nav mt-12" role=tablist>
+                               <a class="active mr-4" href="#pro-details1" data-bs-toggle="tab" role="tab" aria-selected="true">
+                                   <img src="images/thca/Galactic-Gas-1.webp" alt="">
+                               </a>
+                               <a class="mr-4" href="#pro-details2" data-bs-toggle="tab" role="tab" aria-selected="true">
+                                   <img src="images/thca/Galactic-Gas-2.webp" alt="">
+                               </a>
+                               <a class="mr-4" href="#pro-details3" data-bs-toggle="tab" role="tab" aria-selected="true">
+                                   <img src="images/thca/Galactic-Gas-3.webp" alt="">
+                               </a>
+
+                           </div>
+                       </div>
+                   </div>
+               </div>
+               <div class="col-md-12 col-lg-6 col-12">
+                   <div class="product-details-content">
+                       <h3>Galactic Gas - THCA+THCP+HHC+LIVE RESIN 2ML Disposable</h3>
+
+                     <p class="mt-35"><span><a href="#">Login</a> </span>to view price. </p>
+                     <p><span>Availability: </span> Available</p>
+                     <ul>
+                         <li>Our THCA diamond infused distillate live resin disposable comes in a heavy hitter mix of THCA, THCP, and HHC and will be sure to pack a punch!</li>
+                         <li>   Our THCA disposable features a pre-heat button ensuring a clean rip that never gets clogged! And don't worry, we always got you covered with a free charger!</li>
+                        <li>Comes in 3 different incredible flavors – Galactic Gas, Zelato and Jungle Cake</li>
+
+                        </ul>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </div>
+   <div class="product-description-review-area pb-90 mt-35">
+       <div class="container">
+           <div class="product-description-review text-center">
+               <div class="description-review-title nav" role=tablist>
+                   <a class="active" href="#pro-dec" data-bs-toggle="tab" role="tab" aria-selected="true">
+                       Description
+                   </a>
+                   <a href="#pro-review" data-bs-toggle="tab" role="tab" aria-selected="false">
+                       Reviews
+                   </a>
+               </div>
+               <div class="description-review-text tab-content">
+                   <div class="tab-pane active show fade" id="pro-dec" role="tabpanel">
+                     <p>Galactic Gas - THCA+THCP+HHC+LIVE RESIN 2ML Disposable</p>
+                  </div>
+                   <div class="tab-pane fade" id="pro-review" role="tabpanel">
+
+                   </div>
+               </div>
+           </div>
+       </div>
+   </div>
+
+
+
+
+
+
+
+
+ <?php require_once 'footer-product.php';?>
+ <script src="js/owl.carousel.min.js"></script>
+ <script>$('.listPropCat').owlCarousel({
+      nav: true,
+      loop: true,
+      autoplay: true,
+      autoplay:true,
+      autoplayTimeout:3000,
+      autoplayHoverPause:true,
+      margin: 10,
+      responsive: {
+          100: {
+              items: 2
+          },
+          767: {
+              items: 2
+          },
+          1000: {
+              items: 3
+          },
+          1200: {
+              items: 3
+          }
+      }
+
+      });
+   </script>
+
+
+
+  </body>
+</html>
